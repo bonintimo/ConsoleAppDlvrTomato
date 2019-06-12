139123517bf5de642f5570c4f3d6c7db6ddeab9d
bonintimo <bonintimo@gmail.com>
2019-06-12 06:08:12 +0300

    backup

--- ConsoleAppDlvrTomato/Program.cs
+++ ConsoleAppDlvrTomato/Program.cs
@@ -46,8 +46,8 @@ namespace orcplan
     static class MainClass
     {
         
-        public static int MAX_RESTAURANTS_FOR_PLANNING = 2;
-        public static int MAX_COURIERS_FOR_PLANNING = 2;
+        public static int MAX_RESTAURANTS_FOR_PLANNING = 3;
+        public static int MAX_COURIERS_FOR_PLANNING = 3;
         public static int MAX_BEGINING_ORDERS_TO_ADD = 1;
         public static int MAX_ORDERS_FOR_COURIERS = 2;
 
