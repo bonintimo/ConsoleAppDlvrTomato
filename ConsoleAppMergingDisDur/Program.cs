9fab1749d4cb1cf1c062304c910d203e29ca5a4e
bonintimo <bonintimo@gmail.com>
2019-08-29 16:09:19 +0300

    merge dis and dur

--- ConsoleAppMergingDisDur/Program.cs
+++ ConsoleAppMergingDisDur/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleAppMergingDisDur
@@ -10,6 +13,13 @@ namespace ConsoleAppMergingDisDur
     {
         static void Main(string[] args)
         {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
+            DateTime datetimeNow = DateTime.Now;
+
+            OleDbConnection dbConn = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\home91\Downloads\fias_dbf;Extended Properties=dBASE IV;");
+
+            dbConn.Open();
+
         }
     }
 }
