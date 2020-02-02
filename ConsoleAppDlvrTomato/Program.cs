1152f8a2444c573f17445f6356eab141d05ed110
bonintimo <bonintimo@gmail.com>
2020-02-02 07:31:16 +0300

    backup

--- ConsoleAppDlvrTomato/Program.cs
+++ ConsoleAppDlvrTomato/Program.cs
@@ -112,11 +112,11 @@ namespace orcplan
                         break;
 
                     case "INIT":
-                        ReadBgnnOrders(@"./ORDERS-2018-10-19-TM3TM18.tsv");
+                        ReadBgnnOrders(@"./ORDERS-2018-10-18-TM3TM18.tsv");
                         //ReadBgnnOrders(@"./TULA-2018-10-15-TOT.tsv");
                         //ReadBgnnOrders(@"");
                         InitBaseDirForDPR();
-                        deliveryPlan = ReadPlan(@"./tula-all-empty-R3C6.xml");// ReadTestPlan();
+                        deliveryPlan = ReadPlan(@"./tula-all-empty-R3C3.xml");// ReadTestPlan();
                         //deliveryPlan = ReadPlan(@"./tula-all-empty2.xml");// ReadTestPlan();
                         nextPlan = PlanningForOrders(deliveryPlan);
                         break;
